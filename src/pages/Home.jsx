6ca996533b1e60071ceae67c209a165946aa86c4
Ernest Tomas <ernest.tomas@estudiants.urv.cat>
2022-01-27 15:48:27 +0100

    added movie & tv sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,56 @@
 import React from 'react';
+import { OutlineButton } from '../components/button/Button';
 import HeroSlide from '../components/hero-slide/HeroSlide';
-
+import { Link } from 'react-router-dom';
+import MovieList from '../components/movie-list/MovieList';
+import { category, movieType, tvType } from '../api/tmdbApi';
 const Home = () => {
   return (
     <>
       <HeroSlide/>
+      <div className="container">
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Trending Movies</h2>
+            <Link to="/movie">
+              <OutlineButton className="small">View More</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.movie} type={movieType.popular}/>
+
+        </div>
+
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Top rated Movies</h2>
+            <Link to="/movie">
+              <OutlineButton className="small">View More</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.movie} type={movieType.top_rated}/>
+        </div>
+
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Trending TV</h2>
+            <Link to="/movie">
+              <OutlineButton className="small">View More</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.tv} type={tvType.popular}/>
+        </div>
+
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Top rated TV</h2>
+            <Link to="/movie">
+              <OutlineButton className="small">View More</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.tv} type={tvType.top_rated}/>
+        </div>
+
+      </div>
     </>
   );
 };
